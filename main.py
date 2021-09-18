c9f2811fd96e183d55f7085a83d83d6c18ee2481
jyz0309 <45495947@qq.com>
2021-09-18 15:57:48 +0800

    try to change arg
    Signed-off-by: jyz0309 <45495947@qq.com>

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def complex_sqr(z):
 @ti.kernel
 def paint(t: float):
     for i, j in pixels:  # Parallized over all pixels
-        c = ti.Vector([-0.8, ti.cos(t) * 0.2])
+        c = ti.Vector([-0.8, ti.cos(t) * 0.5])
         z = ti.Vector([i / n - 1, j / n - 0.5]) * 2
         iterations = 0
         while z.norm() < 20 and iterations < 50:
